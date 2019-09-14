353d3c6efa13fcc2a2c8d65b5d9cdbdd5173b636
Inspirefly <inspireflyy@gmail.com>
2019-09-14 13:08:59 -0500

    changes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Main */
 let timer;
 let minutes = 25;
 let seconds = 0;
@@ -61,6 +62,23 @@ document.getElementById("reset").addEventListener("click", () => {
     }
 });
 
+/* Settings */
+document.getElementById("open-settings").addEventListener("click", () => {
+    let settings = document.getElementById("settings"); 
+    settings.style.height = "50vh";
+    settings.style.width = "50vw";
+    settings.style.top = "25vh";
+    settings.style.left = "25vw";
+    settings.style.display = "inline-block";
+})
+
+dragElement(document.getElementById("settings"));
+
+document.getElementById("settings-exit").addEventListener("click", () => {
+    document.getElementById("settings-exit").parentNode.parentNode.style.display = "none";
+});
+
+/* Functions */
 function countDown() {
     display_time();
     
@@ -91,6 +109,36 @@ function display_time() {
     document.title = "Pomodone (" + document.getElementById("minutes").innerText + ":" + document.getElementById("seconds").innerText + ")";
 }
 
+function dragElement(element) {
+    let startX = 0, startY = 0, endX = 0, endY = 0;
+    document.getElementById(element.id + "-header").onmousedown = dragMouseDown;
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        startX = e.clientX;
+        startY = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        endX = startX - e.clientX;
+        endY = startY - e.clientY;
+        startX = e.clientX;
+        startY = e.clientY;
+        element.style.left = (element.offsetLeft - endX) + "px";
+        element.style.top = (element.offsetTop - endY) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
 function toggleTimerOn() {
     let timerClassList = document.getElementById("timer").classList;
     timerClassList.toggle("on");
@@ -107,48 +155,3 @@ function toggleTimerOn() {
         clearInterval(timer);
     }
 }
-
-/* w3schools draggle element */
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "-header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-/* Settings */
-dragElement(document.getElementById("settings"));
