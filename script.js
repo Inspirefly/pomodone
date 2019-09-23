6ddfb0677aaff7b9c7b4ff0ed805405f66e1f6f8
Inspirefly <inspireflyy@gmail.com>
2019-09-22 20:47:20 -0500

    Coding style changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+/********/
 /* Main */
+/********/
 let currentTime = document.getElementsByClassName("selected-time")[0];
 let timer;
 let minutes = 25;
@@ -18,284 +20,294 @@ let longMin = document.getElementsByName("long-min")[0].value;
 let longSec = document.getElementsByName("long-sec")[0].value;
 
 document.getElementById("pomo-time").addEventListener("click", () => {
-    minutes = pomoMin;
-    seconds = pomoSec;
+  minutes = pomoMin;
+  seconds = pomoSec;
 
-    display_time();
+  display_time();
 
-    document.getElementById("pomo-time").classList.add("selected-time");
-    document.getElementById("short-time").classList.remove("selected-time");
-    document.getElementById("long-time").classList.remove("selected-time");
+  document.getElementById("pomo-time").classList.add("selected-time");
+  document.getElementById("short-time").classList.remove("selected-time");
+  document.getElementById("long-time").classList.remove("selected-time");
 });
 
 document.getElementById("short-time").addEventListener("click", () => {
-    minutes = shortMin;
-    seconds = shortSec;
+  minutes = shortMin;
+  seconds = shortSec;
 
-    display_time();
+  display_time();
 
-    document.getElementById("pomo-time").classList.remove("selected-time");
-    document.getElementById("short-time").classList.add("selected-time");
-    document.getElementById("long-time").classList.remove("selected-time");
+  document.getElementById("pomo-time").classList.remove("selected-time");
+  document.getElementById("short-time").classList.add("selected-time");
+  document.getElementById("long-time").classList.remove("selected-time");
 });
 
 document.getElementById("long-time").addEventListener("click", () => {
-    minutes = longMin;
-    seconds = longSec;
+  minutes = longMin;
+  seconds = longSec;
 
-    display_time();
+  display_time();
 
-    document.getElementById("pomo-time").classList.remove("selected-time");
-    document.getElementById("short-time").classList.remove("selected-time");
-    document.getElementById("long-time").classList.add("selected-time");
+  document.getElementById("pomo-time").classList.remove("selected-time");
+  document.getElementById("short-time").classList.remove("selected-time");
+  document.getElementById("long-time").classList.add("selected-time");
 });
 
 document.getElementById("clock").addEventListener("click", () => {
-    toggleTimerOn();
+  toggleTimerOn();
 });
 
 document.getElementById("play-pause").addEventListener("click", () => {
-    toggleTimerOn();
+  toggleTimerOn();
 });
 
 document.getElementById("reset").addEventListener("click", resetTime);
 
+/************/
 /* Settings */
+/************/
 document.getElementById("open-settings").addEventListener("click", () => {
-    let settings = document.getElementById("settings"); 
-    settings.style.height = "50vh";
-    settings.style.width = "50vw";
-    settings.style.top = "10em";
-    settings.style.left = "";
-    settings.style.display = "grid";
+  let settings = document.getElementById("settings");
+  settings.style.height = "50vh";
+  settings.style.width = "50vw";
+  settings.style.top = "10em";
+  settings.style.left = "";
+  settings.style.display = "grid";
 
-    /* Times */
-    prettyTimes();
+  /*********/
+  /* Times */
+  /*********/
+  prettyTimes();
 
-    /* Sound */
-    if (sound) {
-        document.getElementById("sound").classList.add("checked");
-    } else {
-        document.getElementById("sound").classList.remove("checked");
-    }
+  /*********/
+  /* Sound */
+  /*********/
+  if (sound) {
+    document.getElementById("sound").classList.add("checked");
+  } else {
+    document.getElementById("sound").classList.remove("checked");
+  }
 
-    /* Cycle */
-    if (cycle) {
-        document.getElementById("cycle").classList.add("checked");
-    } else {
-        document.getElementById("cycle").classList.remove("checked");
-    }
+  /*********/
+  /* Cycle */
+  /*********/
+  if (cycle) {
+    document.getElementById("cycle").classList.add("checked");
+  } else {
+    document.getElementById("cycle").classList.remove("checked");
+  }
 });
 
 dragElement(document.getElementById("settings"));
 
 document.getElementById("settings-confirm").addEventListener("click", () => {
-    /* Times */
-    let valid = true;
+  // Times
+  let valid = true;
 
-    let tempPomoMin = document.getElementsByName("pomo-min")[0].value;
-    let tempPomoSec = document.getElementsByName("pomo-sec")[0].value;
-    let tempShortMin = document.getElementsByName("short-min")[0].value;
-    let tempShortSec = document.getElementsByName("short-sec")[0].value;
-    let tempLongMin = document.getElementsByName("long-min")[0].value;
-    let tempLongSec = document.getElementsByName("long-sec")[0].value;
-    
-    if ((!Number.isNaN(tempPomoMin) && tempPomoMin >= 0) &&
-    (!Number.isNaN(tempPomoSec) && tempPomoSec >= 0 && tempPomoSec < 60) &&
-    (!Number.isNaN(tempShortMin) && tempShortMin >= 0) &&
-    (!Number.isNaN(tempShortSec) && tempShortSec >= 0 && tempShortSec < 60) &&
-    (!Number.isNaN(tempLongMin) && tempLongMin >= 0) &&
-    (!Number.isNaN(tempLongSec) && tempLongSec >= 0 && tempLongSec < 60))
-        valid = true;
-    else
-        valid = false;
+  let tempPomoMin = document.getElementsByName("pomo-min")[0].value;
+  let tempPomoSec = document.getElementsByName("pomo-sec")[0].value;
+  let tempShortMin = document.getElementsByName("short-min")[0].value;
+  let tempShortSec = document.getElementsByName("short-sec")[0].value;
+  let tempLongMin = document.getElementsByName("long-min")[0].value;
+  let tempLongSec = document.getElementsByName("long-sec")[0].value;
 
-    if (valid) {
-        pomoMin = tempPomoMin;
-        pomoSec = tempPomoSec;
-        shortMin = tempShortMin;
-        shortSec = tempShortSec;
-        longMin = tempLongMin;
-        longSec = tempLongSec;
-    } else {
-        alert("Value must be a number and seconds less than 60.");
-    }
+  if ((!Number.isNaN(tempPomoMin) && tempPomoMin >= 0) &&
+  (!Number.isNaN(tempPomoSec) && tempPomoSec >= 0 && tempPomoSec < 60) &&
+  (!Number.isNaN(tempShortMin) && tempShortMin >= 0) &&
+  (!Number.isNaN(tempShortSec) && tempShortSec >= 0 && tempShortSec < 60) &&
+  (!Number.isNaN(tempLongMin) && tempLongMin >= 0) &&
+  (!Number.isNaN(tempLongSec) && tempLongSec >= 0 && tempLongSec < 60))
+    valid = true;
+  else
+    valid = false;
 
-    /* Sound */
-    if (document.getElementById("sound").classList.contains("checked")) {
-        sound = true;
-        audio.volume = 1;
-    } else {
-        sound = false;
-        audio.volume = 0;
-    }
+  if (valid) {
+    pomoMin = tempPomoMin;
+    pomoSec = tempPomoSec;
+    shortMin = tempShortMin;
+    shortSec = tempShortSec;
+    longMin = tempLongMin;
+    longSec = tempLongSec;
+  } else {
+    alert("Value must be a number and seconds less than 60.");
+  }
 
-    /* Cycle */
-    if (document.getElementById("cycle").classList.contains("checked")) {
-        cycle = true;
-    } else {
-        cycle = false;
-        pomos = 0;
-    }
+  // Sound
+  if (document.getElementById("sound").classList.contains("checked")) {
+    sound = true;
+    audio.volume = 1;
+  } else {
+    sound = false;
+    audio.volume = 0;
+  }
 
-    document.getElementById("settings-exit").parentNode.parentNode.style.display = "none";
-    resetTime();
-    display_time();
+  // Cycle
+  if (document.getElementById("cycle").classList.contains("checked")) {
+    cycle = true;
+  } else {
+    cycle = false;
+    pomos = 0;
+  }
+
+  document.getElementById("settings-exit").parentNode.parentNode.style.display = "none";
+  resetTime();
+  display_time();
 });
 
 document.getElementById("settings-exit").addEventListener("click", () => {
-    document.getElementById("settings-exit").parentNode.parentNode.style.display = "none";
+  document.getElementById("settings-exit").parentNode.parentNode.style.display = "none";
 });
 
 const checkboxes = document.getElementsByClassName("checkbox");
 for (let i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener("click", () => {
-        checkboxes[i].classList.toggle("checked");
-    });
+  checkboxes[i].addEventListener("click", () => {
+    checkboxes[i].classList.toggle("checked");
+  });
 }
 
+/*************/
 /* Functions */
+/*************/
 function countDown() {
-    display_time();
-    
-    seconds -= 1;
+  display_time();
 
-    if (seconds == -1) {
-        minutes -= 1;
-        seconds = 59;
-    }
-    
-    if (minutes == -1) {
-        toggleTimerOn();
-        audio.play();
+  seconds -= 1;
 
-        if (cycle) {
-            switch (currentTime.id) {
-                case "pomo-time":
-                    if (pomos < 3) {
-                        pomos += 1;
-                        document.getElementById("short-time").click();
-                    } else {
-                        pomos = 0;
-                        document.getElementById("long-time").click();
-                    }
-                    break;
-                case "short-time":
-                case "long-time":
-                    document.getElementById("pomo-time").click();
-                    break
-            }
-            document.getElementById("play-pause").click();
-        } else {
+  if (seconds == -1) {
+    minutes -= 1;
+    seconds = 59;
+  }
+
+  if (minutes == -1) {
+    toggleTimerOn();
+    audio.play();
+
+    if (cycle) {
+      switch (currentTime.id) {
+        case "pomo-time":
+          if (pomos < 3) {
+            pomos += 1;
+            document.getElementById("short-time").click();
+          } else {
             pomos = 0;
-        }
-    }   
+            document.getElementById("long-time").click();
+          }
+          break;
+        case "short-time":
+        case "long-time":
+          document.getElementById("pomo-time").click();
+          break
+      }
+      document.getElementById("play-pause").click();
+    } else {
+      pomos = 0;
+    }
+  }
 }
 
 function display_time() {
-    if (minutes < 10)
-        document.getElementById("minutes").innerText = "0" + Number(minutes);
-    else
-        document.getElementById("minutes").innerText = Number(minutes);
+  if (minutes < 10)
+    document.getElementById("minutes").innerText = "0" + Number(minutes);
+  else
+    document.getElementById("minutes").innerText = Number(minutes);
 
-    if (seconds < 10)
-        document.getElementById("seconds").innerText = "0" + Number(seconds);
-    else
-        document.getElementById("seconds").innerText = Number(seconds);
+  if (seconds < 10)
+    document.getElementById("seconds").innerText = "0" + Number(seconds);
+  else
+    document.getElementById("seconds").innerText = Number(seconds);
 
-    document.title = "Pomodone (" + document.getElementById("minutes").innerText + ":" + document.getElementById("seconds").innerText + ")";
+  document.title = "Pomodone (" + document.getElementById("minutes").innerText + ":" + document.getElementById("seconds").innerText + ")";
 }
 
 function dragElement(element) {
-    let startX = 0, startY = 0, endX = 0, endY = 0;
-    document.getElementById(element.id + "-header").onmousedown = dragMouseDown;
+  let startX = 0, startY = 0, endX = 0, endY = 0;
+  document.getElementById(element.id + "-header").onmousedown = dragMouseDown;
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        startX = e.clientX;
-        startY = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    startX = e.clientX;
+    startY = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        endX = startX - e.clientX;
-        endY = startY - e.clientY;
-        startX = e.clientX;
-        startY = e.clientY;
-        element.style.left = (element.offsetLeft - endX) + "px";
-        element.style.top = (element.offsetTop - endY) + "px";
-    }
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    endX = startX - e.clientX;
+    endY = startY - e.clientY;
+    startX = e.clientX;
+    startY = e.clientY;
+    element.style.left = (element.offsetLeft - endX) + "px";
+    element.style.top = (element.offsetTop - endY) + "px";
+  }
 
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 function prettyTimes() {
-    if (pomoMin < 10)
-        document.getElementsByName("pomo-min")[0].value = "0" + Number(pomoMin);
-    else
-        document.getElementsByName("pomo-min")[0].value =  Number(pomoMin);
-    if (pomoSec < 10)
-        document.getElementsByName("pomo-sec")[0].value = "0" + Number(pomoSec);
-    else
-        document.getElementsByName("pomo-sec")[0].value = Number(pomoSec);
+  if (pomoMin < 10)
+    document.getElementsByName("pomo-min")[0].value = "0" + Number(pomoMin);
+  else
+    document.getElementsByName("pomo-min")[0].value =  Number(pomoMin);
+  if (pomoSec < 10)
+    document.getElementsByName("pomo-sec")[0].value = "0" + Number(pomoSec);
+  else
+    document.getElementsByName("pomo-sec")[0].value = Number(pomoSec);
 
-    if (shortMin < 10)
-        document.getElementsByName("short-min")[0].value = "0" + Number(shortMin);
-    else
-        document.getElementsByName("short-min")[0].value =  Number(shortMin);
-    if (shortSec < 10)
-        document.getElementsByName("short-sec")[0].value = "0" + Number(shortSec);
-    else
-        document.getElementsByName("short-sec")[0].value = Number(shortSec);
+  if (shortMin < 10)
+    document.getElementsByName("short-min")[0].value = "0" + Number(shortMin);
+  else
+    document.getElementsByName("short-min")[0].value =  Number(shortMin);
+  if (shortSec < 10)
+    document.getElementsByName("short-sec")[0].value = "0" + Number(shortSec);
+  else
+    document.getElementsByName("short-sec")[0].value = Number(shortSec);
 
-    if (longMin < 10)
-        document.getElementsByName("long-min")[0].value = "0" + Number(longMin);
-    else
-        document.getElementsByName("long-min")[0].value =  Number(longMin);
-    if (longSec < 10)
-        document.getElementsByName("long-sec")[0].value = "0" + Number(longSec);
-    else
-        document.getElementsByName("long-sec")[0].value = Number(longSec);
+  if (longMin < 10)
+    document.getElementsByName("long-min")[0].value = "0" + Number(longMin);
+  else
+    document.getElementsByName("long-min")[0].value =  Number(longMin);
+  if (longSec < 10)
+    document.getElementsByName("long-sec")[0].value = "0" + Number(longSec);
+  else
+    document.getElementsByName("long-sec")[0].value = Number(longSec);
 }
 
 function resetTime() {
-    if (document.getElementById("pomo-time").classList.contains("selected-time")) {
-        minutes = pomoMin;
-        seconds = pomoSec;
-        display_time();
-    } else if (document.getElementById("short-time").classList.contains("selected-time")) {
-        minutes = shortMin;
-        seconds = shortSec;
-        display_time();
-    } else {
-        minutes = longMin;
-        seconds = longSec;
-        display_time();
-    }
+  if (document.getElementById("pomo-time").classList.contains("selected-time")) {
+    minutes = pomoMin;
+    seconds = pomoSec;
+    display_time();
+  } else if (document.getElementById("short-time").classList.contains("selected-time")) {
+    minutes = shortMin;
+    seconds = shortSec;
+    display_time();
+  } else {
+    minutes = longMin;
+    seconds = longSec;
+    display_time();
+  }
 }
 
 function toggleTimerOn() {
-    currentTime = document.getElementsByClassName("selected-time")[0];
+  currentTime = document.getElementsByClassName("selected-time")[0];
 
-    let timerClassList = document.getElementById("timer").classList;
-    timerClassList.toggle("on");
+  let timerClassList = document.getElementById("timer").classList;
+  timerClassList.toggle("on");
 
-    let playPauseClassList = document.getElementById("play-pause").classList;
+  let playPauseClassList = document.getElementById("play-pause").classList;
 
-    if (minutes != -1 && timerClassList.contains("on")) {
-        timer = setInterval(countDown, 1000);
-        playPauseClassList.remove("fa-play");
-        playPauseClassList.add("fa-pause");
-    } else {
-        playPauseClassList.remove("fa-pause");
-        playPauseClassList.add("fa-play");
-        clearInterval(timer);
-    }
+  if (minutes != -1 && timerClassList.contains("on")) {
+    timer = setInterval(countDown, 1000);
+    playPauseClassList.remove("fa-play");
+    playPauseClassList.add("fa-pause");
+  } else {
+    playPauseClassList.remove("fa-pause");
+    playPauseClassList.add("fa-play");
+    clearInterval(timer);
+  }
 }
