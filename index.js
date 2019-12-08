2c468d384a8f9b3af75646a5dbcf65bd591fcae6
Inspirefly <inspireflyy@gmail.com>
2019-12-07 18:45:29 -0600

    Fixed PORT

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(500, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
